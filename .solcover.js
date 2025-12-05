53f419888709fc1d5755219177785ea7d15d72ca
Felix Ogucha <fenicfelix@gmail.com>
2025-12-05 06:52:37 +0000

    updates tests cases

--- .solcover.js
+++ .solcover.js
@@ -2,12 +2,12 @@ module.exports = {
   skipFiles: [
     // "DoubleDepositEscrow.sol",
     // "SaferDoubleDepositEscrow.sol",
-    // "RevertingSellerAlwaysFail.sol",
-    // "RevertingBuyer.sol",
-    // "RevertingSeller.sol",
-    // "RevertingReceiver.sol",
-    // "ReentrantAttacker.sol",
-    // "DoubleDepositEscrow_legacy.sol",
-    // "SaferDoubleDepositEscrow_legacy.sol"
+    "RevertingSellerAlwaysFail.sol",
+    "RevertingBuyer.sol",
+    "RevertingSeller.sol",
+    "RevertingReceiver.sol",
+    "ReentrantAttacker.sol",
+    "DoubleDepositEscrow_legacy.sol",
+    "SaferDoubleDepositEscrow_legacy.sol"
   ]
 };
